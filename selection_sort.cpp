86ff916c9df8a948a46a321fb2c39de239bd40df
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-08-13 18:05:42 +0530

    selection sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
 void selection_sort(int arr[],int n){
-    for(int i=0;i<n-2;i++){
+    for(int i=0;i<n-1;i++){
         int mini=i;
-        for(int j = i;j<n-1;j++){
+        for(int j = i;j<n;j++){
             if(arr[j]<arr[mini]){
                 mini=j;
 
             }
-            int temp=arr[mini];
+        }
+         int temp=arr[mini];
             arr[mini]=arr[i];
             arr[i]=temp;
-        }
 
     }
-
+ 
 }
 int main(){
 int n;
