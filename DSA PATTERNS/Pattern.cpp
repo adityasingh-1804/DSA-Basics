73c9f5d2a284b9c44cc989720fffbf85413e1a23
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-08-16 19:43:28 +0530

    sorting practice

--- DSA PATTERNS/Pattern.cpp
+++ DSA PATTERNS/Pattern.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the value of n:";
+    cout<< "enter the value of n";
     cin>> n;
-    for(int i =0;i<=2*n-1;i++){
-        for(int j=0;j<i+1;j++){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
         
-           
-            
-            
+        if(i>n)stars = 2*n-i;
+        for(int j=0;j<=stars;j++){
+        cout<<"*"<<" ";
         }
-    cout<< endl;
+         cout<< endl;
 
-        
     }
-
+    
    
+
 }
