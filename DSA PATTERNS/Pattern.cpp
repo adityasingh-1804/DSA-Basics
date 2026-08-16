9bc0a3df9e4619f4ccdcf22e08f533109e205295
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-08-16 12:38:07 +0530

    patterns question striver sheet

---
+++ DSA PATTERNS/Pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>> n;
+    for(int i =0;i<=2*n-1;i++){
+        for(int j=0;j<i+1;j++){
+        
+           
+            
+            
+        }
+    cout<< endl;
+
+        
+    }
+
+   
+}
