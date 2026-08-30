440dbe1e3436b2a0d619a4b1a8bb06e374270b02
adityasingh-1804 <adityasingh12092007@gmail.com>
2026-08-30 23:41:17 +0530

    array sorted or not

--- DSA PATTERNS/Array/ArrayisSortedORNot.cpp
+++ DSA PATTERNS/Array/ArrayisSortedORNot.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main() {
-    int arr[5] = {1,2,3,4,5};
-
-    for(int i = 1; i < 5; i++) {
-        if(arr[i-1] > arr[i]) {
-            cout << "array is not sorted";
-            return 0;
-        }
+   int arr[9]={1,1,1,2,2,3,3,3,3};
+   int i=0;
+   for(int j = 1; j<9;j++){
+    if(arr[i]!=arr[j]){
+        arr[i+1]=arr[j];
+        i++;
     }
-
-    cout << "array is sorted";
+}
+    cout<<"Array after sorting";
+    for(int j = 0;i<=i;j++){
+        cout<<arr[j]<<" ";
+    }
+return 0;
+   
+   
 }
